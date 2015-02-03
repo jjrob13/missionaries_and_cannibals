7435dac6869139186ea81595f27ed61e61d792c3
Nathaniel <namendoz@asu.edu>
2015-02-03 00:37:02 -0700

    + BFS either can't find a solution, the problem is modeled wrongly in code, or there is coding error

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,15 +9,30 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+#include <deque>
+
+#define STEP_COST 1 //all step costs for this problem is 1
 
 using std::vector;
 using std::set;
 using std::find;
 using std::cout;	using std::endl;
+using std::deque;
 //helper function that generates all of the possible actions that can be performed.
 vector<Action> get_all_actions();
+
 //prototype for test
 void test_check_valid();
+
+//function to generate a child node (not sure where to put this really)
+Node * CHILD_NODE(Problem &, Node *, Action &);
+
+//function to print out the list of actions from the goal state
+void PRINT_SOLUTION(Node *);
+
+bool BREADTH_FIRST_SEARCH(Problem &);
+bool is_in_explored(deque<State> explored, State child_state);
+bool is_in_frontier(deque<Node *> frontier, State child_state);
 
 int main(int argc, const char *argv[])
 {
@@ -27,7 +42,26 @@ int main(int argc, const char *argv[])
 		cout << it->get_direction() << " ; " << it->get_person1() << " ; " << it->get_person2() << endl;
 	}
 
-	test_check_valid();
+	//test_check_valid();
+	
+	//problem set-up
+	State initial_state({3, 0}, {3, 0}, true);
+	Action action(RIGHT, CANNIBAL, NONE);
+	State goal_state({0, 3},{0, 3}, false);
+	Problem main_problem(initial_state, goal_state, all_actions);
+
+	//I WAS JUST TESTING THE is_in_frontier and is_in_explored functions
+
+	State initial_state1({3, 0}, {3, 0}, true);
+	deque<Node*> explored;
+	Action empty;
+	explored.push_back(new Node(main_problem.get_initial_state(), 0, empty, 0));
+
+	cout << ((is_in_frontier(explored, goal_state)) ? "TRUE" : "FALSE") << endl;
+	cout << ((is_in_frontier(explored, initial_state)) ? "TRUE" : "FALSE") << endl;
+
+	BREADTH_FIRST_SEARCH(main_problem);
+
  	return 0;
 }
 
@@ -39,9 +73,8 @@ The Person and Direction enums, which are defined in Action.hpp are repeated her
 
 enum Person{MISSIONARY, CANNIBAL, NONE};
 enum Direction{LEFT, RIGHT};
-
-
 */
+
 vector<Action> get_all_actions(){
 	//vector in which to store all unique actions.
 	vector<Action> result_vector;
@@ -63,6 +96,92 @@ vector<Action> get_all_actions(){
 
 
 	return result_vector;
+}
+/*********** NOTE: 
+if recieving the error "stack smashing detected", add "-fno-stack-protector" to the CMakeLists.txt flags
+
+********************************************/
+bool BREADTH_FIRST_SEARCH(Problem & problem)
+{
+	Action empty_action; //no initial action
+	Node * init_node = new Node(problem.get_initial_state(), 0, empty_action, 0); //initial node created
+	if(problem.goal_test(init_node->get_state())){ PRINT_SOLUTION(init_node); return true;} //if initial state passes, goal has been found
+
+	deque<Node *>frontier; //frontier FIFO queue
+	deque<State>explored; //explored SET 
+	frontier.push_back(init_node); //add initial node to FIFO queue
+	int loopcount =  0;
+	while(true)
+	{
+		if(frontier.empty()){ return false;} //if the frontier is empty and no solution has been found, return false
+
+		Node * node = frontier.front(); frontier.pop_front(); //pop a node in the front of the frontier
+		State node_state = node->get_state(); //grab the state of the popped node
+		explored.push_front(node_state); //add the state to the set
+		
+		vector<Action> problem_actions = problem.get_available_actions_from(node_state); //grab all the possible actions given a state
+		for(vector<Action>::iterator it = problem_actions.begin(); it != problem_actions.end(); it++) //iterate through all of these possible actions
+		{
+			Node *child = CHILD_NODE(problem, node, *it); //generate a child node given the action iterated with
+			State child_state = child->get_state(); //grab the state of the child node
+			if(! ((is_in_explored(explored, child_state)) || (is_in_frontier(frontier, child_state)) ) ) //if the child_state is not in explored or frontier
+			{
+				if(problem.goal_test(child_state)){ PRINT_SOLUTION(child); return true;} //test for the solution
+					
+				frontier.push_back(child);
+			}
+		}
+		cout << "LOOP: " << loopcount << endl; //just as a test to see that the program is still running
+		++loopcount;
+		 
+	}
+
+	return true;
+}
+
+//check if state is in the explored set
+bool is_in_explored(deque<State> explored, State child_state)
+{
+	for(deque<State>::iterator it = explored.begin(); it != explored.end(); it++)
+	{
+		if(child_state == *it) {return true;}
+	}
+
+	return false;
+}
+
+//check if state is in the nodes of the frontier queue
+bool is_in_frontier(deque<Node *> frontier, State child_state)
+{
+	for(deque<Node *>::iterator it = frontier.begin(); it != frontier.end(); it++)
+	{
+		if(child_state == (*it)->get_state()) {return true;}
+	}
+
+	return false;
+}
+
+//generates a new child node given a node, an action
+Node * CHILD_NODE(Problem & current_problem, Node * parent_node, Action & applied_action){
+	State current_state = parent_node->get_state();
+	State new_state = applied_action.perform_action(current_state);
+	int path_cost = parent_node->get_path_cost() + STEP_COST;
+	return new Node(new_state, parent_node, applied_action, path_cost);
+}
+
+//prints the solution actions (backwards for now, change later)
+void PRINT_SOLUTION(Node * final_node)
+{
+	cout << "SOLUTION TO BE PRINTED";
+	string c;
+	std::cin >> c; //temporary pause
+	Node * temp_node = final_node;
+	while(temp_node != 0){
+		Action action_performed = temp_node->get_action();
+		cout << "Action: " + action_performed.to_string() << endl;
+		temp_node = temp_node->get_parent_node();
+	}
+
 }
 
 //no unit testing framework currently, so just using a method to test the check valid action function
